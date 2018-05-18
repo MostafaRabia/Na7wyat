7dca0617eb9a844c678c2115debd6392b6881201
MostafaRabia <mostafarabia64@gmail.com>
2018-05-19 00:54:58 +0200

    change

--- app/Http/Controllers/Exam.php
+++ app/Http/Controllers/Exam.php
@@ -366,7 +366,6 @@ class Exam extends Controller
 		foreach ($getNotFinshArr as $notFinish){
 			if ($notFinish==null){continue;}
 			$getUsersNotFinish = Users::where('id_user',$notFinish->id_user)->where('admin',0)->first();
-			$usersNotFinish[] = $getUsersNotFinish;
 			if ($getUsersNotFinish){$usersNotFinish[] = $getUsersNotFinish;}
 		}
 		app()->singleton('Title',function(){
