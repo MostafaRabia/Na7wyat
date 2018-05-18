b54bd1d180766b7c4d3f6dfae659032890c3a5db
MostafaRabia <mostafarabia64@gmail.com>
2018-05-19 00:47:33 +0200

    session

--- config/session.php
+++ config/session.php
@@ -161,7 +161,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -174,6 +174,6 @@ return [
     |
     */
 
-    'http_only' => true,
+    'http_only' => false,
 
 ];
