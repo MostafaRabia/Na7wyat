5a8d43b63a8e5227f0f9215ce143ce29e5eee7ce
MostafaRabia <mostafarabia64@gmail.com>
2018-05-25 06:36:23 +0200

    change

--- resources/views/users/Header.blade.php
+++ resources/views/users/Header.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta charset="UTF-8"/>
         <meta property="og:image" content="{{app('image')}}/preview.jpg" />
-        <meta property="og:description" content="موقع امتحانات للنحو، برعاية الأُستاذ عُمر إبراهيم والأُستاذ حسن محمد حسن" />
+        <meta property="og:description" content="موقع امتحانات للنحو، برعاية عُمر إبراهيم والباشمهندس حسن محمد حسن" />
         {!! Html::style(app('css').'/materialize.min.css') !!}
         {!! Html::style('https://fonts.googleapis.com/icon?family=Material+Icons') !!}
         {!! Html::style(app('css').'/headerStyle.css') !!}
