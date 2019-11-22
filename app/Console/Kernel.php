0df93bf19296a775e3cc151a7448bec10b644ae8
mr <mostafarabia64@gmail.com>
2019-11-22 18:10:58 +0200

    edit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -76,7 +76,7 @@ class Kernel extends ConsoleKernel
                     }
                 }
             });
-        })->daily()->between('18:00','20:00');
+        })->everyMinute();
         $schedule->command('optimize:clear')->daily();
         $schedule->command('optimize')->daily();
         $schedule->call(function(){
